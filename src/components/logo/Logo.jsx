e6a275266e29196729169beefdab48343f57e523
einartech <imalovelyghost13@gmail.com>
2025-05-12 14:20:41 +0200

    feature(logo):Logo added

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,5 +1,5 @@
 import styles from "./Logo.module.css";
-import logoImage from "../../assets/images/react.svg"; // Importa la imagen del logo
+import logoImage from "../../assets/images/logo/2.png"; // Importa la imagen del logo
 
 export default function Logo() {
   return (
