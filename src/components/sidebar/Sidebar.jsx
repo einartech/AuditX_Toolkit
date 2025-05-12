d5fd71ef2ad9e8cc6deb75c89640c3f6eb6e8f64
einartech <imalovelyghost13@gmail.com>
2025-05-12 12:51:41 +0200

    feature(main-components):Toolkit reusable, responsive, etc

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,29 @@
 import styles from "./Sidebar.module.css";
 
-export default function Sidebar() {
+export default function Sidebar({ setActiveContent }) {
   return (
     <div className={styles.sidebar}>
-      <button className={styles.button}>Nmap</button>
-      <button className={styles.button}>Fuzzing</button>
-      <button className={styles.button}>JWT</button>
+      <button
+        className={styles.button}
+        data-tooltip="Scan networks with Nmap"
+        onClick={() => setActiveContent("nmap")}
+      >
+        Nmap
+      </button>
+      <button
+        className={styles.button}
+        data-tooltip="Find vulnerabilities with Fuzzing"
+        onClick={() => setActiveContent("fuzzing")}
+      >
+        Fuzzing
+      </button>
+      <button
+        className={styles.button}
+        data-tooltip="Manage tokens with JWT"
+        onClick={() => setActiveContent("jwt")}
+      >
+        JWT
+      </button>
     </div>
   );
 }
