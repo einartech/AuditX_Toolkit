35bf46d79fbda17ea07123f5834319325ac47d92
einartech <imalovelyghost13@gmail.com>
2025-05-21 17:18:24 +0200

    text(copryright):improved

--- src/components/copyright/Copyright.jsx
+++ src/components/copyright/Copyright.jsx
@@ -3,7 +3,8 @@ import styles from "./Copyright.module.css";
 export default function Copyright() {
   return (
     <div className={styles.copyright}>
-      © {new Date().getFullYear()} AuditX Toolkit. All rights reserved.
+      © {new Date().getFullYear()} AuditX Toolkit. All rights reserved to
+      Einartech. Powered by Alex, Yeral & Lola from Factoria F5.
     </div>
   );
 }
